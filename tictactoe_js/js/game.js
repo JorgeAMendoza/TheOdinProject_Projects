2ae43eb79f3b82042df99b79f4cb6bb6fd2bbe92
Jorge Mendoza <jorgemendoza2002@gmail.com>
2020-12-12 14:09:25 -0600

    complete controller module, add methods for functions to communicate with each other

--- tictactoe_js/js/game.js
+++ tictactoe_js/js/game.js
@@ -2,28 +2,29 @@ const gameBoardModule = (function (doc) {
   const _domElements = {
     gameBoard: doc.querySelector(".game-board"),
     gameTiles: doc.querySelectorAll(".game-tile"),
-    clearGameButton: doc.querySelector("#clearBoard"),
+    clearBoardButton: doc.querySelector("#clearBoard"),
     gameStatus: doc.querySelector(".game-status"),
   };
-  let _turnCount = 0;
 
-  function _clearBoard(e) {
+  let _turnCount;
+
+  function _clearBoard() {
     _domElements.gameTiles.forEach((tile) => {
       tile.textContent = "";
     });
     _domElements.gameStatus.textContent = "";
+    _domElements.clearBoardButton.disabled = true;
     _domElements.gameTiles.forEach((tiles) =>
       tiles.addEventListener("click", _setMark)
     );
-    _domElements.clearGameButton.disabled = true;
   }
 
   function _setMark(e) {
-    const playerMark = "X";
+    const playerMark = controllerModule.getCurrentPlayerMark();
     e.target.textContent = playerMark;
     e.target.removeEventListener("click", _setMark);
     _turnCount++;
-    _checkWinner();
+    _checkWinner(playerMark);
   }
 
   function _checkLine(line, mark) {
@@ -35,9 +36,9 @@ const gameBoardModule = (function (doc) {
       return true;
   }
 
-  function _checkWinner() {
-    console.log(_turnCount);
-    const playerMark = "X";
+  function _checkWinner(mark) {
+    const playerMark = mark;
+    console.log(playerMark);
     const boardTiles = [..._domElements.gameTiles];
 
     //Set Rows to check
@@ -50,6 +51,8 @@ const gameBoardModule = (function (doc) {
       _checkLine(secondRow, playerMark) ||
       _checkLine(thirdRow, playerMark)
     ) {
+      _domElements.gameStatus.textContent = `${controllerModule.getCurrentPlayerName()} Wins this round!`;
+      _turnCount = 0;
       _setWinner();
       return;
     }
@@ -64,6 +67,8 @@ const gameBoardModule = (function (doc) {
       _checkLine(secondColumn, playerMark) ||
       _checkLine(thirdColumn, playerMark)
     ) {
+      _domElements.gameStatus.textContent = `${controllerModule.getCurrentPlayerName()} Wins this round!`;
+      _turnCount = 0;
       _setWinner();
       return;
     }
@@ -76,36 +81,160 @@ const gameBoardModule = (function (doc) {
       _checkLine(firstDiagnol, playerMark) ||
       _checkLine(secondDiagnol, playerMark)
     ) {
+      _domElements.gameStatus.textContent = `${controllerModule.getCurrentPlayerName()} Wins this round!`;
+      _turnCount = 0;
       _setWinner();
       return;
     }
 
-    if (_turnCount === 9) _tieGame();
+    //Board is full
+    if (_turnCount === 9) {
+      _turnCount = 0;
+      _tieGame();
+    }
   }
 
   function _setWinner() {
     _domElements.gameTiles.forEach((tiles) =>
       tiles.removeEventListener("click", _setMark)
     );
-    window.alert("HELLO");
+    controllerModule.appendScore();
     //(maybe) put line through winner row.
-    //call function to notify container to add score
-    _domElements.clearGameButton.disabled = false;
+    _domElements.clearBoardButton.disabled = false;
   }
 
   function _tieGame() {
     _domElements.gameStatus.textContent = "Tie Game! Play Again.";
-    _domElements.clearGameButton.disabled = false;
+    _domElements.clearBoardButton.disabled = false;
   }
 
-  function _setEvents() {
-    _domElements.clearGameButton.addEventListener("click", _clearBoard);
+  function startGame() {
+    _domElements.clearBoardButton.addEventListener("click", _clearBoard);
     _domElements.gameTiles.forEach((tile) =>
       tile.addEventListener("click", _setMark)
     );
+    _turnCount = 0;
   }
 
-  _setEvents();
+  function endGame() {
+    _domElements.gameTiles.forEach((tile) => {
+      tile.removeEventListener("click", _setMark);
+      tile.textContent = "";
+    });
+    _domElements.clearBoardButton.removeEventListener("click", _clearBoard);
+  }
+
+  return { startGame, endGame };
 })(document);
 
-const controllerModule = (function (doc) {})();
+const controllerModule = (function (doc) {
+  const _domElements = {
+    playerOneName: doc.querySelector("#playerOneName"),
+    playerTwoName: doc.querySelector("#playerTwoName"),
+    startGameButton: doc.querySelector("#startGame"),
+    newGameButton: doc.querySelector("#newGame"),
+    gameMessage: doc.querySelector(".game-message"),
+    playerOneWins: doc.querySelector("#playerOneWins"),
+    playerTwoWins: doc.querySelector("#playerTwoWins"),
+  };
+
+  let _playerOne;
+  let _playerTwo;
+  let _playerTurn = true;
+
+  //player factory function
+  function _player(mark, name) {
+    const _info = {
+      name: name,
+      mark: mark,
+      score: 0,
+    };
+
+    function getName() {
+      return _info.name;
+    }
+    function getMark() {
+      return _info.mark;
+    }
+    function getScore() {
+      return _info.score;
+    }
+    function addScore() {
+      _info.score++;
+    }
+
+    return { getName, getMark, getScore, addScore };
+  }
+
+  function _changeTurn() {
+    console.log(_playerTurn ? "Player one Turn" : "Player Two Turn");
+    _playerTurn = !_playerTurn;
+  }
+
+  function _startGame() {
+    let playerOneName = _domElements.playerOneName.value;
+    let playerTwoName = _domElements.playerTwoName.value;
+
+    if (playerOneName === "" || playerTwoName === "") {
+      window.alert("MAKE SURE BOTH ARE NAMED");
+      return;
+    }
+
+    _playerOne = _player("X", playerOneName);
+    _playerTwo = _player("O", playerTwoName);
+
+    _domElements.startGameButton.disabled = true;
+    _domElements.newGameButton.disabled = false;
+
+    gameBoardModule.startGame();
+  }
+
+  function _newGame() {
+    //Set text name to blank and dislay input boxes.
+    window.alert("STARTING NEW GAME");
+    gameBoardModule.endGame();
+    _domElements.playerOneWins.textContent = "0";
+    _domElements.playerTwoWins.textContent = "0";
+    _domElements.startGameButton.disabled = false;
+  }
+
+  function startController() {
+    _domElements.startGameButton.addEventListener("click", _startGame);
+    _domElements.newGameButton.addEventListener("click", _newGame);
+    _domElements.newGameButton.disabled = true;
+  }
+
+  function appendScore() {
+    if (!_playerTurn) {
+      _playerOne.addScore();
+      _domElements.playerOneWins.textContent = _playerOne.getScore();
+    } else {
+      _playerTwo.addScore();
+      _domElements.playerTwoWins.textContent = _playerTwo.getScore();
+    }
+  }
+
+  function getCurrentPlayerMark() {
+    if (_playerTurn) {
+      _changeTurn();
+      return _playerOne.getMark();
+    } else {
+      _changeTurn();
+      return _playerTwo.getMark();
+    }
+  }
+
+  function getCurrentPlayerName() {
+    if (!_playerTurn) return _playerOne.getName();
+    else return _playerTwo.getName();
+  }
+
+  return {
+    getCurrentPlayerMark,
+    getCurrentPlayerName,
+    appendScore,
+    startController,
+  };
+})(document);
+
+controllerModule.startController();
