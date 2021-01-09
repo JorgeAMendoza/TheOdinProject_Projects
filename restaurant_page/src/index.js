ece2a2e9df05fdd59bfb9fa83576c926cd9c5786
Jorge Mendoza <jorgemendoza2002@gmail.com>
2021-01-08 19:26:42 -0600

    remove html structure, change nav links to buttons
    removed the html there for reference, changed links to buttons so I have to go back and fix styling

--- restaurant_page/src/index.js
+++ restaurant_page/src/index.js
@@ -1,1 +1,24 @@
-console.log("HELLO TACOS");
+import insertHomepage from "./pages/home.js";
+
+(function () {
+  const content = document.querySelector("#content");
+  const homeButton = document.querySelector("#home");
+  const menuButton = document.querySelector("#menu");
+  const aboutMenu = document.querySelector("#about");
+
+  homeButton.addEventListener("click", () => {
+    content.innerHTML = "";
+    content.append(...insertHomepage());
+  });
+
+  menuButton.addEventListener("click", () => {
+    content.innerHTML = "";
+  });
+
+  aboutMenu.addEventListener("click", () => {
+    content.innerHTML = "";
+  });
+
+  //  Start off page
+  content.append(...insertHomepage());
+})();
