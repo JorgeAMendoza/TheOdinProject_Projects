12eecfd4e48a17e1f69f421792d5b45ad663bb15
Jorge Mendoza <jorgemendoza2002@gmail.com>
2021-01-12 20:55:29 -0600

    add underscore JS dependency and throttled functions for scrolling

--- restaurant_page/src/index.js
+++ restaurant_page/src/index.js
@@ -1,3 +1,4 @@
+import underscore from "underscore";
 import insertHomepage from "./pages/home.js";
 import insertMenu from "./pages/menu.js";
 import insertAbout from "./pages/about.js";
@@ -7,6 +8,13 @@ import insertAbout from "./pages/about.js";
   const homeButton = document.querySelector("#home");
   const menuButton = document.querySelector("#menu");
   const aboutMenu = document.querySelector("#about");
+  const navigationBar = document.querySelector(".navbar");
+
+  function changeNavColor() {
+    console.log("Scrolled");
+    if (window.scrollY > 200) navigationBar.classList.add("scrolled");
+    else navigationBar.classList.remove("scrolled");
+  }
 
   homeButton.addEventListener("click", () => {
     content.innerHTML = "";
@@ -22,6 +30,10 @@ import insertAbout from "./pages/about.js";
     content.innerHTML = "";
     content.append(...insertAbout());
   });
+
+  // Set Navigation bar color change
+  const navTransition = underscore.throttle(changeNavColor, 100);
+  window.addEventListener("scroll", navTransition);
 
   //  Start off page injection
   content.append(...insertHomepage());
