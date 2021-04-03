bd7a6986631705e26cb83e4a5da64648d7a22c19
Jorge Mendoza <jorgemendoza2002@gmail.com>
2021-04-03 13:32:44 -0500

    set up webpack configuration files for differnet modes, install necessary webpack plugins needed

--- weather-app/webpack.common.js
+++ weather-app/webpack.common.js
@@ -1,2 +1,23 @@
 const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    assetModuleFilename: "imgs/[hash].[ext]",
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.html$/,
+        use: ["html-loader"],
+      },
+      {
+        test: /\.(svg|png|jpe?g|gif)$/,
+        type: "asset/resource",
+      },
+    ],
+  },
+};
