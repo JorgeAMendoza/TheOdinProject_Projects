6cc277da1dfc2f7789164ae7203ca2375cd39e55
Jorge Mendoza <jorgemendoza2002@gmail.com>
2021-04-05 12:20:43 -0500

    fix scss regex for loaders

--- weather-app/webpack.dev.js
+++ weather-app/webpack.dev.js
@@ -18,7 +18,7 @@ module.exports = merge(common, {
   module: {
     rules: [
       {
-        test: /.\scss$/,
+        test: /\.scss$/,
         use: ["style-loader", "css-loader", "sass-loader"],
       },
     ],
