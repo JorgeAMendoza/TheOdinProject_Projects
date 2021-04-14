0a2edebd8a58155796baedc6f3b249a75faa3a55
Jorge Mendoza <jorgemendoza2002@gmail.com>
2021-04-14 14:30:37 -0500

    adjust getCurrentWeather method to be able to make api calls for new city input, and convert to different units as well

--- weather-app/src/app/weather-data.js
+++ weather-app/src/app/weather-data.js
@@ -4,16 +4,33 @@ import { CurrentWeather } from "./weather-class/current.weather.class";
 export const weatherData = () => {
   const currentWeatherData = new CurrentWeather(0, 0, 0, 0, 0, 0);
   const fireDayForecastData = [];
-  const isMetric = false;
+  let isMetric = false;
   //save this into locale storage???
+  let currentCity = "dallas";
 
   const _setCurrentWeather = async (cityName) => {
-    const currentWeatherData = isMetric
+    currentCity = cityName;
+    const currentWeatherObject = isMetric
       ? await currentWeatherCall(cityName, "metric")
       : await currentWeatherCall(cityName, "imperial");
 
-    // Here, call the method that will reset the values, meaning grab the values from the new weather object,
-    //and pass those into that method.
+    const {
+      status,
+      currentTemp,
+      minTemp,
+      maxTemp,
+      humidity,
+      wind,
+    } = currentWeatherObject;
+
+    currentWeatherData.setNewCurrent(
+      currentTemp,
+      minTemp,
+      maxTemp,
+      status,
+      humidity,
+      wind
+    );
   };
 
   const _setForecast = () => {};
@@ -31,14 +48,26 @@ export const weatherData = () => {
     // Call class method to set all to metric.
   };
 
-  const changeUnit = () => {
-    isMetric = !isMetric;
-  };
-
-  const getCurrentWeather = (city = "Dallas") => {
+  const getCurrentWeather = async (city, unit) => {
     // This is called by the Dom or controller
-    _setCurrentWeather(city);
-    return currentWeatherData;
+    if (currentCity !== city.toLowerCase()) {
+      console.log("MAKING API CALL");
+      // await _setCurrentWeather(city);
+      // console.log(currentWeatherData.getWeatherProperties());
+      // return with the class method call
+    }
+
+    // if unit is metric, set is metric to true, and call metric function
+    if (unit === "metric") {
+      console.log("CONVERTING TO METRIC");
+      isMetric = true;
+      _setToMetric();
+    } else {
+      console.log("CONVERTING TO IMPERIAL");
+      isMetric = false;
+      _setToImperial();
+    }
+    // Return object that only the weather properties.
   };
 
   // (public) funciton that returns five day forecast object/ makes copy and returns it.
@@ -47,9 +76,10 @@ export const weatherData = () => {
     // return fireDayForecast;
   };
 
+  getCurrentWeather("dallas", "metric");
+
   return {
     getCurrentWeather,
     getForecast,
-    changeUnit,
   };
 };
