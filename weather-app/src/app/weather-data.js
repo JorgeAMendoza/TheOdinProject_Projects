bfe5ea3b952ef0266767b96a04feffd3454083d1
Jorge Mendoza <jorgemendoza2002@gmail.com>
2021-04-19 12:21:26 -0500

    module now only returns either error string or new instance of current weather object in the current weather function

--- weather-app/src/app/weather-data.js
+++ weather-app/src/app/weather-data.js
@@ -2,35 +2,21 @@ import { currentWeatherCall } from "./api-calls/current-weather";
 import { CurrentWeather } from "./weather-class/current.weather.class";
 
 export const weatherData = () => {
-  const currentWeatherData = new CurrentWeather(0, 0, 0, 0, 0, 0);
-  const fireDayForecastData = [];
-  let isMetric = false;
-  //save this into locale storage???
-  let currentCity = "dallas";
-
-  const _setCurrentWeather = async (cityName) => {
-    currentCity = cityName;
-    const currentWeatherObject = isMetric
-      ? await currentWeatherCall(cityName, "metric")
-      : await currentWeatherCall(cityName, "imperial");
-
-    const {
-      status,
-      currentTemp,
-      minTemp,
-      maxTemp,
-      humidity,
-      wind,
-    } = currentWeatherObject;
-
-    currentWeatherData.setNewCurrent(
-      currentTemp,
-      minTemp,
-      maxTemp,
-      status,
-      humidity,
-      wind
-    );
+  const _setCurrentWeather = async (cityName, unitType) => {
+    // Make call to weather api, save that to new object.
+    const weatherObject = await currentWeatherCall(cityName, unitType);
+    if (typeof weatherObject === "string") {
+      return weatherObject;
+    } else {
+      return new CurrentWeather(
+        weatherObject.currentTemp,
+        weatherObject.minTemp,
+        weatherObject.maxTemp,
+        weatherObject.status,
+        weatherObject.humidity,
+        weatherObject.wind
+      );
+    }
   };
 
   const _setForecast = () => {};
@@ -40,34 +26,8 @@ export const weatherData = () => {
   // erase the array that has the current five day forecast data.
   // insert the objects into the array.
 
-  const _setToImperial = () => {
-    // Call class method to change all to imperial
-  };
-
-  const _setToMetric = () => {
-    // Call class method to set all to metric.
-  };
-
   const getCurrentWeather = async (city, unit) => {
-    // This is called by the Dom or controller
-    if (currentCity !== city.toLowerCase()) {
-      console.log("MAKING API CALL");
-      // await _setCurrentWeather(city);
-      // console.log(currentWeatherData.getWeatherProperties());
-      // return with the class method call
-    }
-
-    // if unit is metric, set is metric to true, and call metric function
-    if (unit === "metric") {
-      console.log("CONVERTING TO METRIC");
-      isMetric = true;
-      _setToMetric();
-    } else {
-      console.log("CONVERTING TO IMPERIAL");
-      isMetric = false;
-      _setToImperial();
-    }
-    // Return object that only the weather properties.
+    return await _setCurrentWeather("dallas", "metric");
   };
 
   // (public) funciton that returns five day forecast object/ makes copy and returns it.
@@ -76,7 +36,7 @@ export const weatherData = () => {
     // return fireDayForecast;
   };
 
-  getCurrentWeather("dallas", "metric");
+  getCurrentWeather();
 
   return {
     getCurrentWeather,
