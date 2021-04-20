63a587f3b5f77b2caac1f1f42b8dace69ce1c202
Jorge Mendoza <jorgemendoza2002@gmail.com>
2021-04-20 14:15:37 -0500

    create file for forecast weather objects

---
+++ weather-app/src/app/weather-class/forecast.weather.class.js
@@ -0,0 +1,6 @@
+import { convertTempToMetric } from "../unit-conversion-utils/convert-temp-to-metric";
+import { convertTempToImperial } from "../unit-conversion-utils/convert-temp-to-imperial";
+
+export class ForecastWeather {
+    constructor(lowTemp, highTemp, outlook)
+}
