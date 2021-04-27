8c29310b2348f9fe52d35b4c085078d38bbff03a
Jorge Mendoza <jorgemendoza2002@gmail.com>
2021-04-27 10:40:47 -0500

    remove high/low temp properties since api does not return those

--- weather-app/src/app/utils/api-calls/current-weather.js
+++ weather-app/src/app/utils/api-calls/current-weather.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { CurrentWeather } from "../../weather-class/current.weather.class";
 import { grabGeoData } from "../grab-geo-data";
 export const currentWeatherCall = async (cityName, units, state) => {
   try {
@@ -13,13 +14,26 @@ export const currentWeatherCall = async (cityName, units, state) => {
     const targetGeoData = grabGeoData(geoLocationResponse.data, cityName, state);
 
     // console.log(targetGeoData);
-    const { lon: targetLongitude, lat: targetLatitude, name: targetName } = targetGeoData;
+    const { lon: targetLongitude, lat: targetLatitude } = targetGeoData;
     const targetLocation = state ? targetGeoData.state : targetGeoData.country;
 
-    // Pass into one call weather API
-    // Extract the current weather object,and array containing the 7 day forecast.
-    // Create new class from the current weather data,
-    // create new classes from the forecast data.
+    const weatherDataResponse = await axios.get(
+      `https://api.openweathermap.org/data/2.5/onecall?lat=${targetLatitude}&lon=${targetLongitude}&exclude=minutely,hourly,alerts&units=${units}&appid=a4fd7e05b40980c0b9a29ba9590c0fe8`
+    );
+
+    const { current: currentWeatherData } = weatherDataResponse.data;
+    const currentWeather = new CurrentWeather(
+      currentWeatherData.temp,
+      currentWeatherData.weather[0].main,
+      currentWeatherData.weather[0].description,
+      currentWeatherData.humidity,
+      currentWeatherData.wind_speed
+    );
+
+    // Extract the 7 day forecast
+    const { daily: forecastData } = weatherDataResponse.data;
+    forecastData.shift();
+    // iterate over the array, creating a new array that contains instances of the forecast class
     // return an object containing the city name, location (state or country), instance of current weather class, an array which contains an instance of forecast.
   } catch (e) {
     console.log(e.message);
