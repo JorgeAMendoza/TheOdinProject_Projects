4813d3ed38ff85daa4dd97093d13982695c65727
Jorge Mendoza <jorgemendoza2002@gmail.com>
2021-04-27 11:02:56 -0500

    console log reponse to ensure correct data is returned

--- weather-app/src/app/weather-data.js
+++ weather-app/src/app/weather-data.js
@@ -3,7 +3,8 @@ import { CurrentWeather } from "./weather-class/current.weather.class";
 
 export const weatherData = () => {
   const getCurrentWeather = async (city, unit, state = "") => {
-    await currentWeatherCall(city, unit, state);
+    const weatherData = await currentWeatherCall(city, unit, state);
+    console.log(weatherData)
   };
 
   getCurrentWeather("Dallas", "imperial", "TX");
