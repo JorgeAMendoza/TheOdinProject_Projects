9c2ff523ee10c7c2bba83862e3b294f975489934
Jorge Mendoza <jorgemendoza2002@gmail.com>
2021-04-27 10:41:41 -0500

    get current weather data, extract teh current object, and extract and use the properties to create a new instance  of a current weather object

--- weather-app/src/app/weather-class/current.weather.class.js
+++ weather-app/src/app/weather-class/current.weather.class.js
@@ -4,26 +4,21 @@ import { convertWindToMetric } from "../utils/unit-conversion/convert-wind-to-me
 import { convertWindToImperial } from "../utils/unit-conversion/convert-wind-to-imperial";
 
 export class CurrentWeather {
-  constructor(currentTemp, status, minTemp, maxTemp, humidity, wind) {
+  constructor(currentTemp, status, statusDescription, humidity, wind) {
     this.currentTemp = currentTemp;
-    this.minTemp = minTemp;
-    this.maxTemp = maxTemp;
     this.status = status;
+    this.statusDescription = statusDescription;
     this.humidity = humidity;
     this.wind = wind;
   }
 
   setToMetric() {
     this.currentTemp = convertTempToMetric(this.currentTemp);
-    this.minTemp = convertTempToMetric(this.minTemp);
-    this.maxTemp = convertTempToMetric(this.maxTemp);
     this.wind = convertWindToMetric(this.wind);
   }
 
   setToImperial() {
     this.currentTemp = convertTempToImperial(this.currentTemp);
-    this.minTemp = convertTempToImperial(this.minTemp);
-    this.maxTemp = convertTempToImperial(this.maxTemp);
     this.wind = convertWindToImperial(this.wind);
   }
 }
--- weather-app/src/app/weather-data.js
+++ weather-app/src/app/weather-data.js
@@ -3,10 +3,10 @@ import { CurrentWeather } from "./weather-class/current.weather.class";
 
 export const weatherData = () => {
   const getCurrentWeather = async (city, unit, state = "") => {
-    console.log(await currentWeatherCall(city, unit, state));
+    await currentWeatherCall(city, unit, state);
   };
 
-  getCurrentWeather("Dallas", "metric", "TX");
+  getCurrentWeather("Dallas", "imperial", "TX");
 
   return {
     getCurrentWeather,
