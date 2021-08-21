a577ab55ed8f90021ba9c07d8ae412cd04a47cdc
Jorge Mendoza <jorgemendoza2002@gmail.com>
2021-08-21 11:39:50 -0500

    modify the eslint config file, create test files for gameboard

---
+++ battleship/src/test/gameboard/placeship.test.js
@@ -0,0 +1,8 @@
+import { expect } from '@jest/globals';
+import gameboard from '../../app/gameboard/gameboard';
+
+test('Test 1: Placing ship in already occupied space', () => {
+    const gameboard() =>{
+        
+    }
+});
