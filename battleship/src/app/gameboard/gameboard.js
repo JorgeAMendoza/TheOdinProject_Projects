dfb8d06067afb8b5db06724a66edfa95f754dfcd
Jorge Mendoza <jorgemendoza2002@gmail.com>
2021-08-23 19:09:12 -0500

    being crafting the gameboard module, starting with changing the placement direction.

--- battleship/src/app/gameboard/gameboard.js
+++ battleship/src/app/gameboard/gameboard.js
@@ -1,17 +1,31 @@
-export default gameboard = () => {
-  const shipBoard = Array.from(Array(8), () => new Array(8)).fill(0);
+export default function gameboard() {
+  const shipBoard = Array(8)
+    .fill()
+    .map(() => Array(8).fill(0));
   const ships = [];
   let shipPlacementDirection = 'x';
   //   Somehow keep track of all current ships, be able to iterate over them and call isSunkFunciton, but only if a ship was just sunk.
-  // Function that takes in a ship module, and places it down on the board.
-  const allSunk = () => {};
+
+  const changePlacementDirection = () => {
+    if (shipPlacementDirection === 'x') shipPlacementDirection = 'y';
+    else shipPlacementDirection = 'x';
+  };
+
+  const getPlacementDirection = () => shipPlacementDirection;
 
   const placeShip = (ship, x, y) => {
-    // Check array and see if there is a a ship already there, basically not null,
-    // if the first coordindate is zero, then check up or down depending on axis and length of theip.
+    const shipLength = ship.getLength;
+    // if the first coo rdindate is zero, then check up or down depending on axis and length of theip.
     // if it hits anything else than 0, then throw an error.
     // else, place the ship.
   };
+
+  const isTaken = (x, y) => {
+    if (shipBoard[x][y] !== 0) return true;
+  };
+  // Function that takes in a ship module, and places it down on the board.
+  const allSunk = () => {};
+
   // private method to check if placing down is out of bounds.
   const checkSpace = () => {};
   // reciedveAttack takes in corodinates,
@@ -23,4 +37,9 @@ export default gameboard = () => {
 
   //   Private method to call if attack was unncessful, changes value to -1,
   const markSpot = (status) => {};
-};
+
+  return {
+    changePlacementDirection,
+    getPlacementDirection,
+  };
+}
