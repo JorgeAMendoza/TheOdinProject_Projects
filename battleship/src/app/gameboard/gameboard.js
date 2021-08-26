db3df6ba96ac6b73a640d8a43146110a2540fcb6
Jorge Mendoza <jorgemendoza2002@gmail.com>
2021-08-25 19:10:22 -0500

    push all test so far with gameboard module partially built

--- battleship/src/app/gameboard/gameboard.js
+++ battleship/src/app/gameboard/gameboard.js
@@ -3,43 +3,75 @@ export default function gameboard() {
     .fill()
     .map(() => Array(8).fill(0));
   const ships = [];
-  let shipPlacementDirection = 'x';
-  //   Somehow keep track of all current ships, be able to iterate over them and call isSunkFunciton, but only if a ship was just sunk.
-
-  const changePlacementDirection = () => {
-    if (shipPlacementDirection === 'x') shipPlacementDirection = 'y';
-    else shipPlacementDirection = 'x';
-  };
-
-  const getPlacementDirection = () => shipPlacementDirection;
-
-  const placeShip = (ship, x, y) => {
-    const shipLength = ship.getLength;
-    // if the first coo rdindate is zero, then check up or down depending on axis and length of theip.
-    // if it hits anything else than 0, then throw an error.
-    // else, place the ship.
-  };
 
   const isTaken = (x, y) => {
     if (shipBoard[x][y] !== 0) return true;
+    return false;
   };
+
+  const canBePlaced = (shipLength, xStart, yStart, direction) => {
+    let xCord = xStart;
+    let yCord = yStart;
+    if (direction === 'x') {
+      for (let i = 0; i < shipLength; i += 1) {
+        if (xCord > 7 || isTaken(xCord, yCord)) return false;
+        xCord += 1;
+      }
+    } else {
+      for (let i = 0; i < shipLength; i += 1) {
+        if (yCord > 7 || isTaken(xCord, yCord)) return false;
+        yCord += 1;
+      }
+    }
+
+    return true;
+  };
+
+  const placeShip = (ship, x, y, direction) => {
+    const shipLength = ship.getLength();
+    let xCord = x;
+    let yCord = y;
+    if (!canBePlaced(shipLength, x, y, direction)) return false;
+    // Logic to then push objectst that correspond to the ship objects.
+    for (let i = 0; i < shipLength; i += 1) {
+      shipBoard[xCord][yCord] = { index: i, ship };
+      if (direction === 'x') xCord += 1;
+      else yCord += 1;
+    }
+
+    ships.push(ship);
+    return true;
+  };
+
+  const receiveAttack = (x, y) => {
+    if (shipBoard[x][y] === 0) {
+      shipBoard[x][y] = -1;
+      return false;
+    }
+
+    const shipSpot = shipBoard[x][y].index;
+    if (!shipBoard[x][y].ship.hit(shipSpot)) return false;
+
+    shipBoard[x][y] = 1;
+    return true;
+  };
+  // If coordinates are something, then check if its hit,
+  // if not hit, then hit,
+  // Check if current ship is fully sunk.
+
+  // private function that checks if its possible to place down the ship.
   // Function that takes in a ship module, and places it down on the board.
   const allSunk = () => {};
 
   // private method to check if placing down is out of bounds.
   const checkSpace = () => {};
   // reciedveAttack takes in corodinates,
-  const receiveAttack = (x, y) => {};
-  // If coordinates are 0, then return
-  // If coordinates are something, then check if its hit,
-  // if not hit, then hit,
-  // Check if current ship is fully sunk.
 
   //   Private method to call if attack was unncessful, changes value to -1,
   const markSpot = (status) => {};
 
   return {
-    changePlacementDirection,
-    getPlacementDirection,
+    placeShip,
+    receiveAttack,
   };
 }
--- battleship/src/test/gameboard.test.js
+++ battleship/src/test/gameboard.test.js
@@ -4,29 +4,44 @@ import ship from '../app/ship/ship';
 
 describe('Testing game board module', () => {
   const testGameBoard = gameboard();
-  const testShipOne = ('Carrier', 5);
-  const testShipTwo = ('Battleship', 4);
+  const testShipOne = ship('Carrier', 5);
+  const testShipTwo = ship('Battleship', 4);
 
-  test('Test 2: Change direction of the placement of the ship', () => {
-    testGameBoard.changePlacementDirection();
-    expect(testGameBoard.getPlacementDirection()).toBe('y');
+  test('Test 1: Out of bounds placement returns false on x direction', () => {
+    expect(testGameBoard.placeShip(testShipOne, 7, 0, 'x')).toBe(false);
   });
 
-  // test('Test 2: Placing a ship regularly should return true', () => {
-  //   expect(testGameBoard.placeShip(testShipOne, 1, 1)).toBe(true);
-  // });
+  test('Test 2: Out of bounds placement returns false on y axis', () => {
+    expect(testGameBoard.placeShip(testShipOne, 0, 7, 'y')).toBe(false);
+  });
 
-  // test('Test 3: Placing a ship in a taken spot should return false', () => {
-  //   expect(testGameBoard.placeShip(testShipTwo, 1, 1)).toBe(false);
-  // });
+  test('Test 3: in bounds placement returns true on x axis', () => {
+    expect(testGameBoard.placeShip(testShipOne, 2, 4, 'x')).toBe(true);
+  });
 
-  // test('Test 4: Sending a successful attack should return true', () => {
-  //   expect(testGameBoard.recieveAttack(1, 1)).toBe(true);
-  // });
+  test('Test 4: in bounds placement returns true on y axis', () => {
+    expect(testGameBoard.placeShip(testShipTwo, 0, 0, 'y')).toBe(true);
+  });
 
-  // test('Test 5: Sending a unsucessful attack should return false', () => {
-  //   expect(testGameBoard.recieveAttack(1, 1)).toBe(false);
-  // });
+  test('Test 5: Placing a ship in a taken spot should return false on x axis', () => {
+    expect(testGameBoard.placeShip(testShipTwo, 2, 3)).toBe(false);
+  });
+
+  test('Test 6: Placing a ship in a taken spot should return false on the y axis', () => {
+    expect(testGameBoard.placeShip(testShipOne, 0, 2)).toBe(false);
+  });
+
+  test('Test 5: Sending a unsucessful attack should return false', () => {
+    expect(testGameBoard.receiveAttack(1, 1)).toBe(false);
+  });
+
+  test('Test 6: Sending a succesfull attack should return true', () => {
+    expect(testGameBoard.receiveAttack(0, 0)).toBe(true);
+  });
+
+  test('Test 7: Hitting an already hit spot returns false', () => {
+    expect(testGameBoard.receiveAttack(0, 0)).toBe(false);
+  });
 
   // test('Test 6: If all ships sunk, it should return true', () => {
   //   expect(testGameBoard.allShipsSunk()).toBe(true);
