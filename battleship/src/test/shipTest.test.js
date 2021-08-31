b1f0d7a11deaa6fb0a8a51f98e3db9df8d1ac37f
Jorge Mendoza <jorgemendoza2002@gmail.com>
2021-08-31 18:06:47 -0500

    change test method to it method which is better for these test scenarios

--- battleship/src/test/shipTest.test.js
+++ battleship/src/test/shipTest.test.js
@@ -1,10 +1,10 @@
-import { describe, expect } from '@jest/globals';
+import { describe, expect, it } from '@jest/globals';
 import ship from '../app/ship/ship';
 
 describe('Testing Ship Module', () => {
   const testShip = ship('Carrier', 5);
 
-  test('Test 1: Initalizing ship should return a ship status of all points as false', () => {
+  it('Test 1: Initalizing ship should return a ship status of all points as false', () => {
     expect(testShip.getShipStatus()).toStrictEqual([
       false,
       false,
@@ -14,7 +14,7 @@ describe('Testing Ship Module', () => {
     ]);
   });
 
-  test('Test 1: Hitting an empty space should show the index as true', () => {
+  it('Test 1: Hitting an empty space should show the index as true', () => {
     testShip.hit(0);
     expect(testShip.getShipStatus()).toStrictEqual([
       true,
@@ -25,7 +25,7 @@ describe('Testing Ship Module', () => {
     ]);
   });
 
-  test('Test 3: Hitting a taken space should not effect the ship data points at all', () => {
+  it('Test 3: Hitting a taken space should not effect the ship data points at all', () => {
     testShip.hit(0);
     expect(testShip.getShipStatus()).toStrictEqual([
       true,
@@ -36,15 +36,15 @@ describe('Testing Ship Module', () => {
     ]);
   });
 
-  test('Test 4: isSunk should return false if not all points have been hit', () => {
+  it('Test 4: isSunk should return false if not all points have been hit', () => {
     expect(testShip.isSunk()).toBe(false);
   });
 
-  test('Test 5: Return the correct ship length', () => {
+  it('Test 5: Return the correct ship length', () => {
     expect(testShip.getLength()).toBe(5);
   });
 
-  test('Test 6: Returns true if all points are hit', () => {
+  it('Test 6: Returns true if all points are hit', () => {
     testShip.hit(1);
     testShip.hit(2);
     testShip.hit(3);
