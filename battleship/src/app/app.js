d05fbd28ce90e391a2b32f2768d244507846ef86
Jorge Mendoza <jorgemendoza2002@gmail.com>
2021-10-01 18:18:20 -0500

    write plans for placing ships

--- battleship/src/app/app.js
+++ battleship/src/app/app.js
@@ -16,15 +16,25 @@ export default function app() {
   };
 
   const renderPlayerSetup = () => {
-    // Call function to write playersetup on the page
-    console.log('Rendering playersetup');
     const gameContainer = document.querySelector('.container');
     setupScreen(gameContainer);
     gameContainer.classList.add('container--game-active');
     staticDOM.gameBody.classList.add('body--game-active');
     gameContainer.classList.remove('container--set-content');
-    
-    
+
+    // Set events.
+
+    // So we are going to need to grab each game piece, and the arrow that is going to move from ship to ship. 
+
+    // fOR spaces 
+    // Const grab each one. 
+    // Iterate over the node list to set event listner. 
+    // For a hover event, set a grey square, "hover in and hover out"
+    // For hover in, set text content to grey circle. 
+    // on click, check to see if space is valid, if not then place a red dot and write the error message. 
+    // On hover out, wipe markup and repeat process. 
+    // if spot is valid, then remove error message, grab spaces that belong in that row, (x or y) and write blue circles within then. 
+    // Then move the arrow to the next ship, and repeat the process until we have exhouasted the ship icon array.  
   };
 
   const renderGameStart = () => {
