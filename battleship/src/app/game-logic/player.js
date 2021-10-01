ed4fa78ea04efc39dbb78cc18e84cda19253ede5
Jorge Mendoza <jorgemendoza2002@gmail.com>
2021-10-01 18:52:49 -0500

    refacotr and complete player module

--- battleship/src/app/game-logic/player.js
+++ battleship/src/app/game-logic/player.js
@@ -1,20 +1,23 @@
-export default function player(name, playerBoard, opponentBoard) {
+export default function player(name, board, oppBoard) {
   const playerName = name;
+  const playerBoard = board;
+  const opponentBoard = oppBoard;
 
-  // placeship, places ship on the gameboard, returns false if not possible.
   const placeShip = (ship, x, y, direction) =>
     playerBoard.placeShip(ship, x, y, direction);
 
-  // attackOponent
   // calls the oponenet board recieve attack with coordinates
   const sendAttack = (x, y) => opponentBoard.receiveAttack(x, y);
 
-  // didLastShipSink, should check if the last ship coordinate ship sunk. *
-
   // didPlayerWin, calls the gamebaord allSunk function.
+  const didPlayerWin = () => opponentBoard.allSunk();
+
+  const getPlayerName = () => playerName;
 
   return {
     placeShip,
     sendAttack,
+    getPlayerName,
+    didPlayerWin,
   };
 }
