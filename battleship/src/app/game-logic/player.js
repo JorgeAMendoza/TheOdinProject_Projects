18bb6d972cbf1ff9bd4a619e103e4e378f5bf3be
Jorge Mendoza <jorgemendoza2002@gmail.com>
2021-10-04 20:21:15 -0500

    refactor player module
    change the player module to keep track of its own gameboard only, and instead of firing off attacks, it recieves attacks to send to its own gameboard

--- battleship/src/app/game-logic/player.js
+++ battleship/src/app/game-logic/player.js
@@ -1,23 +1,17 @@
-export default function player(name, board, oppBoard) {
+export default function player(name, board) {
   const playerName = name;
   const playerBoard = board;
-  const opponentBoard = oppBoard;
 
   const placeShip = (ship, x, y, direction) =>
     playerBoard.placeShip(ship, x, y, direction);
 
-  // calls the oponenet board recieve attack with coordinates
-  const sendAttack = (x, y) => opponentBoard.receiveAttack(x, y);
-
-  // didPlayerWin, calls the gamebaord allSunk function.
-  const didPlayerWin = () => opponentBoard.allSunk();
+  const getAttack = (x, y) => playerBoard.receiveAttack(x, y);
 
   const getPlayerName = () => playerName;
 
   return {
     placeShip,
-    sendAttack,
     getPlayerName,
-    didPlayerWin,
+    getAttack,
   };
 }
