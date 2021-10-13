65bba8cf8d87829e8e49269b0a328486f2e842f1
Jorge Mendoza <jorgemendoza2002@gmail.com>
2021-10-12 19:17:07 -0500

    set classes for placing ships
    instead of writing in the game pieces, we add a class that displays the piece

--- battleship/src/app/components/render-game-setup.js
+++ battleship/src/app/components/render-game-setup.js
@@ -1,6 +1,7 @@
 import writeSetupScreen from './helpers/write-setup-screen';
 import ship from '../game-logic/ship';
 import greyGamePiece from '../../assets/icons/other/grey-game-piece.svg';
+import redGamepiece from '../../assets/icons/other/red-game-piece.svg';
 
 const shiftArrowIcon = (arrow, shipIcon) => {
   const shipPosition = shipIcon.getBoundingClientRect();
@@ -50,27 +51,72 @@ const renderGameSetup = (domTarget, playerData, OpponentData) => {
         !playerInfo.canPlace(shipArray[shipIndex], xCord, yCord, shipDirection)
       ) {
         canPlace = false;
+        e.target.classList.add('placement-board__board-piece--red');
         return;
       }
       canPlace = true;
-      e.target.innerHTML = `<img src=${greyGamePiece} class="placement-board__game-piece">`;
+      // e.target.innerHTML = `<img src=${greyGamePiece} class="placement-board__game-piece">`;
+      // Now we need to write the grey square corresponding to the the direction.
+      // So if the current direction is x, need to grab the current y coordinate and iterate until its
+      // the current coordinate plus 5.
+      // So we will no longer need the event target but the board pieces.
+      for (
+        let i = yCord;
+        i < yCord + shipArray[shipIndex].getLength();
+        i += 1
+      ) {
+        // Get the current Ycord and xcord, in this case i is y.
+        const writePieceY = i;
+        const boardPiece = xCord * 10 + writePieceY;
+        boardPieces[boardPiece].classList.add(
+          'placement-board__board-piece--grey'
+        );
+      }
+
+      // Same thing for other direciton, but instead with the x direciton.
     })
   );
 
   boardPieces.forEach((piece) =>
     piece.addEventListener('mouseleave', (e) => {
-      e.target.style.setProperty('background', 'white');
+      if (!canPlace) {
+        e.target.classList.remove('placement-board__board-piece--red');
+        return;
+      }
+      const [xCord, yCord] = e.target.dataset.coordinate
+        .split(',')
+        .map((cord) => Number(cord));
+      for (
+        let i = yCord;
+        i < yCord + shipArray[shipIndex].getLength();
+        i += 1
+      ) {
+        // Get the current Ycord and xcord, in this case i is y.
+        const writePieceY = i;
+        const boardPiece = xCord * 10 + writePieceY;
+        boardPieces[boardPiece].classList.remove(
+          'placement-board__board-piece--grey'
+        );
+      }
     })
   );
 
   boardPieces.forEach((piece) =>
-    piece.addEventListener('click', () => {
+    piece.addEventListener('click', (e) => {
       if (!canPlace) return;
+      console.log(e);
+      e.stopImmediatePropagation();
+      console.log(e.target);
+      console.log(e.target.dataset.coordinate);
+      // const [xCord, yCord] = e.target.dataset.coordinate
+      //   .split(',')
+      //   .map((cord) => Number(cord));
 
-      // Grab the coordinates again, and over these pieces, depending on the direction, iterate over the relative pieces and write a blue square, then at the same time REMOVE the event listener so that we dont overwrite over these blue square if we hover them again.
-      // Then move the arrow to the next ship, and repeat the process until we have exhouasted the ship icon array.
-      shipIndex += 1;
-      shiftArrowIcon(arrowPointer, shipIcons[shipIndex]);
+      // console.log(xCord, yCord);
+
+      // // Then move the arrow to the next ship, and repeat the process until we have exhouasted the ship icon array.
+      // shipIndex += 1;
+      // shiftArrowIcon(arrowPointer, shipIcons[shipIndex]);
     })
   );
 
