652a27855bda75ade6592f9538ce2a9a8edb5e91
Jorge Mendoza <jorgemendoza2002@gmail.com>
2021-10-20 16:18:30 -0500

    add event listener for sending attack
    add event listener for sending attack to opponent board, so far we can console log the result of a move

--- battleship/src/app/components/render-game-area.js
+++ battleship/src/app/components/render-game-area.js
@@ -4,9 +4,33 @@ const renderGameArea = (domTarget, player, opponent) => {
   const gameBody = domTarget;
   const gameContainer = gameBody.querySelector('.container');
   writeAreaScreen(gameContainer);
+  const playerBoardDOM = gameBody.querySelectorAll(
+    '#playerBoard .placement-board__board-piece'
+  );
+  const opponentBoardDOM = gameBody.querySelectorAll(
+    '#enemyBoard .placement-board__board-piece'
+  );
 
   const playerData = player;
   const opponentData = opponent;
+
+  const playerGameboard = playerData.getBoard();
+  const opponentGameBoard = opponentData.getBoard();
+
+  const sendAttack = (xCord, yCord, attackedPlayer) => {
+    const moveResult = attackedPlayer.getAttack(xCord, yCord);
+    console.log(moveResult);
+  };
+
+  opponentBoardDOM.forEach((piece) => {
+    const [xCord, yCord] = piece.dataset.coordinate
+      .split(',')
+      .map((cord) => +cord);
+
+    piece.addEventListener('click', () =>
+      sendAttack(xCord, yCord, opponentData)
+    );
+  });
 
   gameContainer.classList.remove('container--set-content');
 };
