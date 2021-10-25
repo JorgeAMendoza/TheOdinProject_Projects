b13222bb39f22dca69e317c1c60e125a4fd5b4c2
Jorge Mendoza <jorgemendoza2002@gmail.com>
2021-10-25 17:27:54 -0500

    opponent can send attacks to player board and go again if the last attack was a success

--- battleship/src/app/components/render-game-area.js
+++ battleship/src/app/components/render-game-area.js
@@ -13,13 +13,58 @@ const renderGameArea = (domTarget, player, opponent) => {
 
   const playerData = player;
   const opponentData = opponent;
+  const opponentMoves = {};
+  let playerTurn = true;
 
-  const sendAttack = (e) => {
+  const opponentSendAttack = () => {
+    let xCord = Math.floor(Math.random() * 10);
+    let yCord = Math.floor(Math.random() * 10);
+
+    while (opponentMoves[`${xCord},${yCord}`]) {
+      xCord = Math.floor(Math.random() * 10);
+      yCord = Math.floor(Math.random() * 10);
+    }
+
+    opponentMoves[`${xCord},${yCord}`] = true;
+    const playerDOMCord = xCord * 10 + (yCord % 10);
+    const moveResult = playerData.getAttack(xCord, yCord);
+
+    switch (moveResult) {
+      case 'hit':
+        playerBoardDOM[playerDOMCord].classList.add(
+          'placement-board__board-piece--red'
+        );
+        // If its a hit, then then we call the opponent turn again,
+        // in it really should matter, but we'll return instead of break.
+        console.log('Sending another attack');
+        opponentSendAttack();
+        break;
+
+      case 'sunk':
+        playerBoardDOM[playerDOMCord].classList.add(
+          'placement-board__board-piece--red'
+        );
+        // grabbing the current player. call the win function/lose function.
+        // if win, then call render function with corrent arguments.
+        break;
+
+      default:
+        playerBoardDOM[playerDOMCord].classList.add(
+          'placement-board__board-piece--grey'
+        );
+        break;
+    }
+
+    playerTurn = true;
+  };
+
+  const playerSendAttack = (e) => {
+    if (!playerTurn) return;
     const [xCord, yCord] = e.target.dataset.coordinate
       .split(',')
       .map((cord) => +cord);
     const moveResult = opponentData.getAttack(xCord, yCord);
-    e.target.removeEventListener('click', sendAttack);
+    e.target.removeEventListener('click', playerSendAttack);
 
     switch (moveResult) {
       case 'hit':
@@ -36,10 +81,13 @@ const renderGameArea = (domTarget, player, opponent) => {
         e.target.classList.add('placement-board__board-piece--grey');
         break;
     }
+
+    playerTurn = false;
+    opponentSendAttack();
   };
 
   opponentBoardDOM.forEach((piece) => {
-    piece.addEventListener('click', sendAttack);
+    piece.addEventListener('click', playerSendAttack);
   });
 
   gameContainer.classList.remove('container--set-content');
